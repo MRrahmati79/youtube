7e639adf0ce42cb69d0df826642002b940af34a7
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-09-22 11:09:35 +0330

    set title and favicon in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Youtube.com',
-  description: 'its a personal project in next js ',
-}
+  title: "youtube.com",
+  description: "its a personal project in next js and Designed by Mojtaba Ahmadpour",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }

b31d91a2e8873fb91ac6b4df084f609e6226fa01
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-03 19:56:17 +0330

    set popover and avatar in notif and user

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,14 +1,40 @@
+"use client"
+import { UserOutlined , BellOutlined  } from "@ant-design/icons";
+import React, { useState } from "react";
+import { Avatar, Button, Popover } from "antd";
+
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const hide = () => {
+    setOpen(false);
+  };
+  const handleOpenChange = (newOpen) => {
+    setOpen(newOpen);
+  };
   return (
     <div className="flex">
-      <div className="w-3/4 flex">
+      <div className="w-3/4 flex justify-around items-center">
         <div className="">serch bar</div>
         <div className="">mic</div>
       </div>
-      <div className="w-1/4 flex">
+      <div className="w-1/4 flex  justify-around items-center">
         <div className="">camera</div>
-        <div className="">notificatio</div>
-        <div className="">avatar</div>
+        <div className="">
+          {" "}
+          <Popover
+            content={<a onClick={hide}>Close</a>}
+            title="Title"
+            trigger="click"
+            open={open}
+            onOpenChange={handleOpenChange}
+          >
+            <Avatar size="large" icon={<BellOutlined />} />
+          </Popover>
+          
+        </div>
+        <div className="">
+          <Avatar size="large" icon={<UserOutlined />} />
+        </div>
       </div>
     </div>
   );

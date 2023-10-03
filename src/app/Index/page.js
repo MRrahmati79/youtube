1637b00a2a4cb7003b6c0d9a74ad3bee2161a856
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-03 19:37:20 +0330

    set divs in header

--- src/app/Index/page.js
+++ src/app/Index/page.js
@@ -4,11 +4,11 @@ import Topics from "../components/Topics";
 import Main from "../components/Main";
 export default function Index() {
   return (
-    <div className="flex">
-      <div className="">
+    <div className="flex m-4">
+      <div className="w-1/5 ">
         <Sidebar />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col w-4/5 ">
         <Header />
         <Topics />
         <Main />
--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,8 +1,15 @@
-
 export default function Header() {
   return (
-    <div className="">
-      Header
+    <div className="flex">
+      <div className="w-3/4 flex">
+        <div className="">serch bar</div>
+        <div className="">mic</div>
+      </div>
+      <div className="w-1/4 flex">
+        <div className="">camera</div>
+        <div className="">notificatio</div>
+        <div className="">avatar</div>
+      </div>
     </div>
   );
 }

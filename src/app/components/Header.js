ab808a1fd14cdc18ac0d6df1389c506bd9bff894
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-07 19:05:04 +0330

    remove all popovers

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,16 +1,6 @@
 "use client";
-import { UserOutlined, BellOutlined } from "@ant-design/icons";
-import React, { useState } from "react";
-import { Avatar, Popover } from "antd";
 
 export default function Header() {
-  const [open, setOpen] = useState(false);
-  const hide = () => {
-    setOpen(false);
-  };
-  const handleOpenChange = (newOpen) => {
-    setOpen(newOpen);
-  };
   return (
     <div className="flex">
       <div className="w-3/4 flex justify-around items-center">
@@ -19,20 +9,8 @@ export default function Header() {
       </div>
       <div className="w-1/4 flex  justify-around items-center">
         <div className="">camera</div>
-        <div className="">
-          <Popover
-            content={<a onClick={hide}>Close</a>}
-            title="Title"
-            trigger="click"
-            open={open}
-            onOpenChange={handleOpenChange}
-          >
-            <Avatar size="large" icon={<BellOutlined />} />
-          </Popover>
-        </div>
-        <div className="">
-          <Avatar size="large" icon={<UserOutlined />} />
-        </div>
+        <div className="">notification</div>
+        <div className="">User icon</div>
       </div>
     </div>
   );

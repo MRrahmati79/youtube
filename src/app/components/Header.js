56a399232cd4dd1c3db6b37f50e988ec5950b8be
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-10-07 19:34:48 +0330

    set popover and icons

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,6 +1,35 @@
 "use client";
+import React, { useState } from "react";
+import { Popover } from "antd";
 
 export default function Header() {
+  
+//function for cameara icon 
+const [open, setOpen] = useState(false);
+const hide = () => {
+  setOpen(false);
+};
+const camIcon = (newOpen) => {
+  setOpen(newOpen);
+};
+
+//function for bell Icon
+const [show, setShow] = useState(false);
+const unShow = () => {
+  setOpen(false);
+};
+const notification = (newOpen) => {
+  setOpen(newOpen);
+};
+
+//function for user icon
+const [openUserIcon, setopenUserIcon] = useState(false);
+const closeUserIconMenu = () => {
+  setOpen(false);
+};
+const userIconMeun = (newOpen) => {
+  setOpen(newOpen);
+};
   return (
     <div className="flex">
       <div className="w-3/4 flex justify-around items-center">
@@ -8,9 +37,40 @@ export default function Header() {
         <div className="">mic</div>
       </div>
       <div className="w-1/4 flex  justify-around items-center">
-        <div className="">camera</div>
-        <div className="">notification</div>
-        <div className="">User icon</div>
+        <div className="">
+          {" "}
+          <Popover
+            content={<a onClick={hide}>Close</a>}
+            title="Title"
+            trigger="click"
+            open={open}
+            onOpenChange={camIcon}
+          >
+            <img src="/assets/icons/camIcon.svg" />
+          </Popover>
+        </div>
+        <div className="">
+        <Popover
+            content={<a onClick={unShow}>Close</a>}
+            title="Title"
+            trigger="click"
+            open={open}
+            onOpenChange={notification}
+          >
+            <img src="/assets/icons/bellIcon.svg" />
+          </Popover>
+        </div>
+        <div className="">
+        <Popover
+            content={<a onClick={closeUserIconMenu}>Close</a>}
+            title="Title"
+            trigger="click"
+            open={open}
+            onOpenChange={userIconMeun}
+          >
+            <img src="/assets/icons/userIcon.svg" />
+          </Popover>
+        </div>
       </div>
     </div>
   );
